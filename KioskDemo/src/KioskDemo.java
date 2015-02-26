db5a76fc24bfb68f2459c630659dba1d5073574d
forando <forando@gmail.com>
2015-02-26 13:53:06 +0200

    KioskDemo Module added

---
+++ KioskDemo/src/KioskDemo.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015. This code is a LogosProg property. All Rights Reserved.
+ */
+
+/**
+ * Created by forando on 26.02.15.
+ */
+public class KioskDemo {
+    public static void main(String[] args) {
+        new HostServer();
+        new DisplayForm();
+    }
+}
